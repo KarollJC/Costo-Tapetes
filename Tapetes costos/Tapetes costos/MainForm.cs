f5c964d38736660d1ab5e590c991ca2e0b135369
KarollJC <juck080419hgtrvra0@cbtis217.edu.mx>
2024-11-20 07:43:31 -0600

    Proyecto creado

---
+++ Tapetes costos/Tapetes costos/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC24
+ * Date: 20/11/2024
+ * Time: 07:26 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Tapetes_costos
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
